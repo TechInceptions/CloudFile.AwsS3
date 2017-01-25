337c328c2d434a5d8ddc84d4a75cc1fe54d4d1ef
Arinze <arinze@techinceptions.com>
2017-01-25 14:25:29 -0500

    initial commit

---
+++ AWSS3Sample/S3Access.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHIS.CloudFile;
+
+namespace AWSS3Sample
+{
+    public class S3Access
+    {
+        private Amazon.S3.AmazonS3Client _S3Client;
+
+        public void Connect(string containerUri, Encoding encoding = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(string azureStorageConnectionString, string containerName, Encoding encoding = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
