d7ace844747d72ed88e20e89272581059f09a667
Arinze Ufoeze <arinze@techinceptions.com>
2018-07-07 16:16:30 -0400

    removed old project

--- TECHIS.CloudFile.AwsS3/Errors.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TECHIS.CloudFile.AwsS3
-{
-    internal class Errors
-    {
-        internal const string ConnectionStringInvalid = "The connection string must contain two parts: the key and the secret";
-        internal const string ServiceClientObjectInvalid = "The client object is invalid. Did you forget to call connect?";
-    }
-}
--- TECHIS.CloudFile.AwsS3/GlobalConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TECHIS.CloudFile.AwsS3
-{
-    internal static class GlobalConstants
-    {
-        internal static char[] KeySeperator = { ',', '|' };
-    }
-}
